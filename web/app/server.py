c3b0d88a7dcb94aa35d213db6b0657d7bfc4f2ff
Alexandr <97986811+AleksandrFrom@users.noreply.github.com>
2023-06-11 00:01:52 +0300

    web model

---
+++ web/app/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+
+# загружаем модель из файла
+
+with open('/app/models/model.pkl', 'rb') as pkl_file:
+    model = pickle.load(pkl_file)
+
+# создаём приложение
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    msg = "Test message. The server is running"
+    return msg
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = np.array(request.json)
+    features = features.reshape(1, 17)
+    prediction = model.predict(features)
+    return  jsonify({'prediction': float(prediction[0])})
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
+    
